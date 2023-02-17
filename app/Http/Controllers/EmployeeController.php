649059321e7d5b56bda26d412a08d6f73d659dd6
unknown <sam_pazoki@yahoo.com>
2023-02-16 23:50:16 -0300

    making the api and server

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index()
+    {
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+
 }
